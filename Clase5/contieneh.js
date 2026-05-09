ee83d1d69d6900b2457cfebac139e2166ff41fbf
Edwin Paredes <paredesvasqueze@iestptrujillo.net>
2026-05-08 19:06:43 -0500

    prueba 3

--- Clase5/contieneh.js
+++ Clase5/contieneh.js
@@ -1,9 +1,9 @@
 nombre = prompt("Ingresar nombre");
 nombre = nombre.toLowerCase();
-posicion = nombre.indexOf("h");
+posicion = nombre.indexOf("e");
 
-if(posicion == -1) {
-  alert("No contiene H");
+if(posicion != -1) {
+  alert("El nombre empieza con e");
 } else {
-  alert("Contiene h");
+  alert("no empieza con e");
 }
