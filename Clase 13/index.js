9d48a38501839c0ab8d33305e2cb6077313b8596
Edwin Paredes <paredesvasqueze@iestptrujillo.net>
2026-06-24 16:45:47 -0500

    cambios

---
+++ Clase 13/index.js
@@ -0,0 +1,32 @@
+const midiv = document.getElementById("holamundo");
+
+function cambiaraazul()
+{
+
+midiv.style.color = "blue";
+}
+
+function cambiaraverde()
+{
+midiv.style.color = "green";
+}
+
+function cambiararojo()
+{
+midiv.style.backgroundColor = "red";
+}
+
+function cambiaraamarillo()
+{
+midiv.style.backgroundColor = "yellow";
+}
+
+function perdiste()
+{
+    alert("perdiste");
+}
+
+function mostrarmensaje()
+{
+    alert("moviste el mouse dentro");
+}
