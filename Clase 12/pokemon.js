a9a10f332688559cd87f04af26a9a8470661ee17
Edwin Paredes <paredesvasqueze@iestptrujillo.net>
2026-07-01 18:11:05 -0500

    Objetos v2

--- Clase 12/pokemon.js
+++ Clase 12/pokemon.js
@@ -1,14 +1,21 @@
+const miimagen = document.getElementById('imgpokemon');
+
+
+
 async function buscarPokemon()
 {
     let respuesta =
     await fetch(
-        "https://pokeapi.co/api/v2/pokemon/pikachu"
+        "https://pokeapi.co/api/v2/pokemon/ivysaur"
     );
 
     let pokemon =
     await respuesta.json();
 
     console.log(pokemon);
+    console.log(pokemon.abilities[1].ability.name);
+    console.log(pokemon.abilities[0].is_hidden);
+    miimagen.src = pokemon.sprites.front_default;
 }
 
 buscarPokemon();
